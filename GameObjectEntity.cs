40b39345b4d76a552840fb83268375e3f542767c
DESKTOP-UJRR6FS\Admin <tuduchoang@gmail.com>
2023-03-29 22:05:33 +0700

    1.0.2

--- GameObjectEntity.cs
+++ GameObjectEntity.cs
@@ -2,8 +2,20 @@ using UnityEngine;
 
 namespace Entities
 {
-    public class GameObjectEntity : MonoBehaviour, IEntity
+    public class GameObjectEntity : IEntity
     {
-        public int InstanceId => GetInstanceID();
+        private GameObject _impl;
+
+        public GameObject Impl
+        {
+            set => _impl = value;
+        }
+
+        public int InstanceId => _impl.GetInstanceID();
+
+        public T GetComponent<T>()
+        {
+            return _impl.GetComponent<T>();
+        }
     }
 }
